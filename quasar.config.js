53a443970caf8e9ce81ca4e72f3932d56ef6e0c8
Artem Shuvaev <shuvaevlol@gmail.com>
2022-11-06 17:03:53 +0500

    chore(config): add sentry to boot config

--- quasar.config.js
+++ quasar.config.js
@@ -30,7 +30,7 @@ module.exports = configure((/* ctx */) => ({
   // app boot file (/src/boot)
   // --> boot files are part of "main.js"
   // https://v2.quasar.dev/quasar-cli-vite/boot-files
-  boot: ['i18n', 'axios'],
+  boot: ['i18n', 'axios', 'sentry'],
 
   // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
   css: ['app.scss'],
