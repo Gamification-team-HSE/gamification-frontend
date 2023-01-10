485a026cc4edeae51892328882d0c93418e70774
Artem Shuvaev <shuvaevlol@gmail.com>
2023-01-11 00:44:37 +0500

    chore: change stats page

--- src/pages/StatsPage.vue
+++ src/pages/StatsPage.vue
@@ -1,9 +1,5 @@
 <template>
   <q-page class="row items-center justify-evenly">
     stats
-    <q-img
-      width="256px"
-      src="http://vartose.fvds.ru:9002/avatars/test.png"
-    />
   </q-page>
 </template>
