8112b3a9055f96ae46cf404de6d19ec6955956e0
Artem Shuvaev <shuvaevlol@gmail.com>
2023-01-11 00:36:32 +0500

    chore(img): test image for stats page

--- src/pages/StatsPage.vue
+++ src/pages/StatsPage.vue
@@ -1,5 +1,9 @@
 <template>
   <q-page class="row items-center justify-evenly">
     stats
+    <q-img
+      width="256px"
+      src="http://vartose.fvds.ru:9002/avatars/test.png"
+    />
   </q-page>
 </template>
